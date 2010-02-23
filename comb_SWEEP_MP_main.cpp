42a670dfe1f26577b9630dea517c8b2d7d420f53
ernie <erniejazz1984@gmail.com>
2010-02-23 15:16:56 +0800

    Merge with branch

--- comb_SWEEP_MP_main.cpp
+++ comb_SWEEP_MP_main.cpp
@@ -4,8 +4,8 @@ extern int sweep();
 int main()
 {
 
-    for(int i=0;i<5;i++)
-      sweep(40,500000,200,1/pow(10,6),50,12,200,600,0.005*2*pi*i);
+//    for(int i=0;i<5;i++)
+      sweep(2,500000,200,1/pow(10,5),50,12,200,600,0);
       //sweep steps, total steps, power(uW/cm2),convergence condition,convergency steps,ADM order,n1 interval,n2 interval,laser detune
 
 return 0;
