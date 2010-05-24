ccbb2c875e4be1aa1587a12da3ea50e01fd13687
Ernie <erniejazz1984@gmail.com>
2010-05-24 14:38:12 +0800

    Add two fitting python file for CW and Comb respectively.

--- fit_CW.py
+++ fit_CW.py
@@ -5,21 +5,13 @@ from pylab import *
 from scipy import *
 from scipy.optimize import leastsq
 #import sys
-<<<<<<< HEAD
-filename = 'data.txt'
+filename = 'data_CW.txt'
 
 g2 = ['%g' %(0.5*200*(i+1)) for i in range(5)]
 print g2
 
 def fit(line):
   filename = 'CW_g2_20_LW_0.7_dt_0.01_%s_uWcm2_1e-10_conS_10_O=12.txt' %line
-=======
-
-g2 = ['%g' %(20.0/2**(i)) for i in range(9)]
-
-def fit(line):
-  filename = 'CW_g2_%s_LW_0.7_dt_0.01_5000_uWcm2_1e-10_conS_10_O=12.txt' %line
->>>>>>> c2708de87bb33bfe2255409564cfce30b0d0d6cf
   print filename
   #filename = sys.argv[1]
   data = loadtxt(filename,skiprows=1)
@@ -27,7 +19,6 @@ def fit(line):
   y = data[:,1]
   x = x*1e7
   y = y*1e10
-<<<<<<< HEAD
   fit_func = lambda p,x : p[0]+p[3]*(p[2]/2/pi)/((x-p[1])**2+(p[2]/2)**2)+p[4]*x
   error_func = lambda p,x,y : fit_func(p,x)-y
   p0 = [ 1000 , 0 , 1 , -50, 0]
@@ -63,22 +54,3 @@ for element in a:
 data.close()
 #data.write('')
 #savetxt('data.txt', a)
-=======
-  fit_func = lambda p,x : p[0]+p[3]*(p[2]/2/pi)/((x-p[1])**2+(p[2]/2)**2) 
-  error_func = lambda p,x,y : fit_func(p,x)-y
-  p0 = [ 5 , 0 , 1 , -10]
-  p1,sucess = leastsq(error_func,p0[:],args=(x,y), maxfev=10000)
-  xfit = linspace(min(x),max(x), 1001)
-  plot(x,y,'.')
-  plot(xfit,fit_func(p1,xfit),'r-')
-  savefig('CW_g2_%s_LW_0.7_dt_0.01_5000_uWcm2_1e-10_conS_10_O=12.png'%line) 
-  clf()
-  pfinal=r_[float(line),p1]
-  return pfinal
-
-a = [fit(gg) for gg in g2]
-#ll = array(['detune','y_offset','x_offset','width','peak'])
-#a.insert(0,ll)
-print a
-savetxt('data.txt', a)
->>>>>>> c2708de87bb33bfe2255409564cfce30b0d0d6cf
---
+++ fit_comb.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from numpy import loadtxt
+from pylab import *
+from scipy import *
+from scipy.optimize import leastsq
+#import sys
+filename = 'data_comb.txt'
+
+g2 = ['%g' %(200*(i+1)) for i in range(5)]
+print g2
+
+def fit(line):
+  filename = 'comb_g2_20_LW_0.7GHz_%suWcm2_1e-10_conS_10_O=12_N1_10_N2_600_D_0MHz_S.txt' %line
+  print filename
+  #filename = sys.argv[1]
+  data = loadtxt(filename,skiprows=1)
+  x = data[:,0]
+  y = data[:,1]
+  x = (x-0.09192631)*1e7
+  y = y*1e10
+  fit_func = lambda p,x : p[0]+p[3]*(p[2]/2/pi)/((x-p[1])**2+(p[2]/2)**2)
+  error_func = lambda p,x,y : fit_func(p,x)-y
+  p0 = [ 500 , 0 , 1 , -500]
+  p1,sucess = leastsq(error_func,p0[:],args=(x,y), maxfev=50000)
+  xfit = linspace(min(x),max(x), 1001)
+  plot(x,y,'.')
+  plot(xfit,fit_func(p1,xfit),'r-')
+  savefig('comb_g2_20_LW_0.7GHz_%suWcm2_1e-10_conS_10_O=12_N1_10_N2_600_D_0MHz_S.png'%line) 
+  clf()
+  p1[0]=p1[0]/1e10
+  p1[1]=p1[1]/1e5
+  p1[2]=p1[2]/1e5
+  p1[3]=p1[3]/1e10
+  pfinal=r_[float(line),p1]
+  return pfinal
+
+a = [fit(gg) for gg in g2]
+
+data = open( filename, 'w')
+ll = ['detune(uW)\t','y_offset\t','x_offset(Hz)\t','width(Hz)\t','peak\t']
+
+for name in ll:
+ data.write(name)
+
+data.write('\n')
+
+for element in a:
+
+ for value in element:
+  data.write('%g\t' %value)   
+ data.write('\n')
+
+data.close()
